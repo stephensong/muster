b5d80c3a4c8558e98df0f22988ddff547887e922
David Wragg <david.wragg@db.com>
2019-02-11 14:39:21 +0000

    Be explicit about release note label

--- packages/website/siteConfig.js
+++ packages/website/siteConfig.js
@@ -35,7 +35,7 @@ const siteConfig = {
     { href: `${baseUrl || '/'}playground/`, label: 'Playground' },
     { doc: 'resources/faq', label: 'FAQ' },
     { page: 'help', label: 'Help' },
-    { blog: true, label: 'Blog' },
+    { blog: true, label: 'Notes' },
     { href: repoUrl, label: 'GitHub' },
   ],
 
